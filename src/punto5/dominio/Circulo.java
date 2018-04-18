405df81b3b6c1da7ce3b05f7915466fd2b97f3f5
getoconas <getoconas@gmail.com>
2018-04-18 02:50:13 -0300

    Modificacion de estructura punto 5.

---
+++ src/punto5/dominio/Circulo.java
@@ -0,0 +1,23 @@
+package punto5.dominio;
+
+/**/
+
+public class Circulo {
+    private double radio;
+    
+    public Circulo() {        
+    }
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+    
+    public double getRadio() {
+        return radio;
+    }
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    public double getSuperficie() {
+        return Math.PI * Math.pow(this.getRadio(), 2);
+    } 
+}
---
+++ src/punto5/util/GestorCirculo.java
@@ -0,0 +1,25 @@
+package punto5.util;
+
+import java.util.ArrayList;
+import punto5.dominio.Circulo;
+
+/**/
+
+public class GestorCirculo {
+    private ArrayList<Circulo> listaCirculos=new ArrayList<Circulo>(); //Agregar el new por el NullPointException
+
+    public ArrayList<Circulo> getListaCirculos() {
+        return listaCirculos;
+    }
+
+    public void setListaCirculos(ArrayList<Circulo> listaCirculos) {
+        this.listaCirculos = listaCirculos;
+    }
+    
+    public void addCirculo(Circulo circulo) {
+        this.listaCirculos.add(circulo);
+    }
+    public int cantidad(){
+        return this.listaCirculos.size();
+    }
+}
